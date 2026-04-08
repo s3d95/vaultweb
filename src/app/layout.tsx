d320a7fdc92c407ff896abd92632ef13263dbe2c
Saad Hawareen <113633648+s3d95@users.noreply.github.com>
2026-04-08 21:56:33 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,7 @@ export default function RootLayout({
       >
         <Script
           id="adsense-script"
-          async
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3525357385323761"
           crossOrigin="anonymous"
         />
